51b4cb66b5f86eb86f5cef80741728c7a3e65b6c
Tristan Lins <tristan.lins@bit3.de>
2013-03-04 12:35:41 +0100

    Fix assets path.

--- src/system/modules/!composer/config/config.php
+++ src/system/modules/!composer/config/config.php
@@ -85,6 +85,6 @@ EOF;
  */
 $GLOBALS['BE_MOD']['system']['composer'] = array(
 	'callback'   => 'ComposerClientBackend',
-	'icon'       => 'system/modules/composer-client/assets/images/icon.png',
-	'stylesheet' => 'system/modules/composer-client/assets/css/backend.css',
+	'icon'       => 'system/modules/!composer/assets/images/icon.png',
+	'stylesheet' => 'system/modules/!composer/assets/css/backend.css',
 );
