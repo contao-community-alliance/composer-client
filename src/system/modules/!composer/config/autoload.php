d029b59e46580a6d88bc76e0733da4f4b3697783
Tristan Lins <tristan.lins@bit3.de>
2013-03-04 12:34:28 +0100

    Add autoloader files.

---
+++ src/system/modules/!composer/config/autoload.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2013 Leo Feyer
+ * 
+ * @package !composer
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'ComposerClientBackend' => 'system/modules/!composer/ComposerClientBackend.php',
+));
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'be_composer_client'                  => 'system/modules/!composer/templates',
+	'be_composer_client_editor'           => 'system/modules/!composer/templates',
+	'be_composer_client_form'             => 'system/modules/!composer/templates',
+	'be_composer_client_ftp_mode'         => 'system/modules/!composer/templates',
+	'be_composer_client_install'          => 'system/modules/!composer/templates',
+	'be_composer_client_install_composer' => 'system/modules/!composer/templates',
+	'be_composer_client_search'           => 'system/modules/!composer/templates',
+));
