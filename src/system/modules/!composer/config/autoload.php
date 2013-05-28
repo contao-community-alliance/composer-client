0816ee52ae3f505721fb6b5167a93b87f575215f
Tristan Lins <tristan.lins@bit3.de>
2013-05-28 15:34:36 +0200

    Update autoload.php

--- src/system/modules/!composer/config/autoload.php
+++ src/system/modules/!composer/config/autoload.php
@@ -2,9 +2,9 @@
 
 /**
  * Contao Open Source CMS
- * 
- * Copyright (C) 2005-2013 Leo Feyer
- * 
+ *
+ * Copyright (c) 2005-2013 Leo Feyer
+ *
  * @package !composer
  * @link    https://contao.org
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
@@ -16,6 +16,7 @@
  */
 ClassLoader::addClasses(array
 (
+	'ComposerClient'        => 'system/modules/!composer/ComposerClient.php',
 	'ComposerClientBackend' => 'system/modules/!composer/ComposerClientBackend.php',
 ));
 
@@ -25,13 +26,14 @@ ClassLoader::addClasses(array
  */
 TemplateLoader::addFiles(array
 (
-	'be_composer_client'                  => 'system/modules/!composer/templates',
-	'be_composer_client_dependency_graph' => 'system/modules/!composer/templates',
-	'be_composer_client_editor'           => 'system/modules/!composer/templates',
+	'be_composer_client_search'           => 'system/modules/!composer/templates',
 	'be_composer_client_errors'           => 'system/modules/!composer/templates',
-	'be_composer_client_form'             => 'system/modules/!composer/templates',
+	'be_composer_client_solve'            => 'system/modules/!composer/templates',
 	'be_composer_client_install'          => 'system/modules/!composer/templates',
 	'be_composer_client_install_composer' => 'system/modules/!composer/templates',
-	'be_composer_client_search'           => 'system/modules/!composer/templates',
 	'be_composer_client_update'           => 'system/modules/!composer/templates',
+	'be_composer_client'                  => 'system/modules/!composer/templates',
+	'be_composer_client_dependency_graph' => 'system/modules/!composer/templates',
+	'be_composer_client_form'             => 'system/modules/!composer/templates',
+	'be_composer_client_editor'           => 'system/modules/!composer/templates',
 ));
