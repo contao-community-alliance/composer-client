2fecee6cb328867efcc471a77e0b13979a855b68
Tristan Lins <tristan.lins@bit3.de>
2013-11-05 18:54:55 +0100

    Extract convenience method Runtime::increaseMemoryLimit().

--- src/system/modules/!composer/ContaoCommunityAlliance/Contao/Composer/ClientBackend.php
+++ src/system/modules/!composer/ContaoCommunityAlliance/Contao/Composer/ClientBackend.php
@@ -280,7 +280,7 @@ class ClientBackend extends \BackendModule
 		}
 
 		// try to increase memory limit
-		$this->increaseMemoryLimit();
+		Runtime::increaseMemoryLimit();
 
 		// register composer class loader
 		if (file_exists(TL_ROOT . '/composer/vendor/composer/composer/src/Composer/Composer.php') &&
@@ -326,45 +326,6 @@ class ClientBackend extends \BackendModule
 
 		// assign composer to template
 		$this->Template->composer = $this->composer;
-	}
-
-	/**
-	 * Try to increase memory.
-	 */
-	protected function increaseMemoryLimit()
-	{
-		/**
-		 * Copyright (c) 2011 Nils Adermann, Jordi Boggiano
-		 *
-		 * @see https://github.com/composer/composer/blob/master/bin/composer
-		 */
-		if (function_exists('ini_set')) {
-			@ini_set('display_errors', 1);
-
-			$memoryInBytes = function ($value) {
-				$unit  = strtolower(substr($value, -1, 1));
-				$value = (int) $value;
-				switch ($unit) {
-					case 'g':
-						$value *= 1024;
-					// no break (cumulative multiplier)
-					case 'm':
-						$value *= 1024;
-					// no break (cumulative multiplier)
-					case 'k':
-						$value *= 1024;
-				}
-
-				return $value;
-			};
-
-			$memoryLimit = trim(ini_get('memory_limit'));
-			// Increase memory_limit if it is lower than 512M
-			if ($memoryLimit != -1 && $memoryInBytes($memoryLimit) < 512 * 1024 * 1024) {
-				@ini_set('memory_limit', '512M');
-			}
-			unset($memoryInBytes, $memoryLimit);
-		}
 	}
 
 	/**
--- src/system/modules/!composer/ContaoCommunityAlliance/Contao/Composer/Runtime.php
+++ src/system/modules/!composer/ContaoCommunityAlliance/Contao/Composer/Runtime.php
@@ -47,4 +47,42 @@ class Runtime
 		Downloader::download($url, $file);
 		return true;
 	}
+
+	/**
+	 * Try to increase memory.
+	 *
+	 * Inspired by Nils Adermann, Jordi Boggiano
+	 *
+	 * @see https://github.com/composer/composer/blob/master/bin/composer
+	 */
+	static public function increaseMemoryLimit()
+	{
+		if (function_exists('ini_set')) {
+			@ini_set('display_errors', 1);
+
+			$memoryInBytes = function ($value) {
+				$unit  = strtolower(substr($value, -1, 1));
+				$value = (int) $value;
+				switch ($unit) {
+					case 'g':
+						$value *= 1024;
+					// no break (cumulative multiplier)
+					case 'm':
+						$value *= 1024;
+					// no break (cumulative multiplier)
+					case 'k':
+						$value *= 1024;
+				}
+
+				return $value;
+			};
+
+			$memoryLimit = trim(ini_get('memory_limit'));
+			// Increase memory_limit if it is lower than 512M
+			if ($memoryLimit != -1 && $memoryInBytes($memoryLimit) < 512 * 1024 * 1024) {
+				@ini_set('memory_limit', '512M');
+			}
+			unset($memoryInBytes, $memoryLimit);
+		}
+	}
 }
