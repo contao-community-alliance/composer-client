77fa98c38f60410e3e17e8a1a307a78fb5f73b36
Contao Community Alliance <info@c-c-a.org>
2014-05-14 14:45:33 +0200

    Auto-update translations from transifex

---
+++ src/system/modules/!composer/languages/de/tl_settings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/composer/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-05-09T03:00:20+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_settings']['composerExecutionMode']['0'] = 'Ausführungsmodus';
+$GLOBALS['TL_LANG']['tl_settings']['composerPhpPath']['0']       = 'PHP-Pfad/Befehl';
+$GLOBALS['TL_LANG']['tl_settings']['composerPhpPath']['1']       = 'Der Pfad oder Befehl zum PHP-Binary.';
+$GLOBALS['TL_LANG']['tl_settings']['composer_legend']            = 'Composer-Einstellungen';
+
