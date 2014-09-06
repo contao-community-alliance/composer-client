265194272c627eabd65e97101328bcc5340543cc
Tristan Lins <tristan.lins@bit3.de>
2014-09-07 01:34:36 +0200

    Fix languages files to pass code style checks.

--- src/system/modules/!composer/languages/en/tl_settings.php
+++ src/system/modules/!composer/languages/en/tl_settings.php
@@ -17,12 +17,14 @@ $GLOBALS['TL_LANG']['tl_settings']['composerPhpPath']                = array(
 );
 $GLOBALS['TL_LANG']['tl_settings']['composerRemoveRepositoryTables'] = array(
     'Remove repository client tables',
-    'The old ER2 repository client tables will not be removed by the composer client database update tool, until you enable this checkbox.'
+    'The old ER2 repository client tables will not be removed by the composer client database update tool, ' .
+    'until you enable this checkbox.'
 );
 
 $GLOBALS['TL_LANG']['tl_settings']['composerVerbosity'] = array(
     'Verbosity in logging',
-    'The console has 5 levels of verbosity, usually you will want to leave this to the default verbosity unless you experience problems.'
+    'The console has 5 levels of verbosity, ' .
+    'usually you will want to leave this to the default verbosity unless you experience problems.'
 );
 
 
@@ -31,15 +33,20 @@ $GLOBALS['TL_LANG']['tl_settings']['composerVerbosity'] = array(
  */
 $GLOBALS['TL_LANG']['tl_settings']['composerExecutionModes']['inline']   = array(
     'In the HTTP request.',
-    'Composer will get executed within the web server process. This mode is usually slower but works for all systems but is subject to the maximum runtime limitations of PHP.'
+    'Composer will get executed within the web server process. ' .
+    'This mode is usually slower but works for all systems but is subject to the maximum runtime limitations of PHP.'
 );
 $GLOBALS['TL_LANG']['tl_settings']['composerExecutionModes']['process']  = array(
     'as sub process of the web server process',
-    'Composer will get executed via sub process call as external program. This is usually faster but only possible on systems supporting proc_open() and is subject to the maximum runtime limitations of PHP.'
+    'Composer will get executed via sub process call as external program. ' .
+    'This is usually faster but only possible on systems supporting proc_open() and ' .
+    'is subject to the maximum runtime limitations of PHP.'
 );
 $GLOBALS['TL_LANG']['tl_settings']['composerExecutionModes']['detached'] = array(
     'as standalone process',
-    'Composer will get executed as standalone sub process and detached into the background. This is not possible or allowed on some systems (please check with your provider if it is allowed to spawn background processes). This method has nearly no limitations.'
+    'Composer will get executed as standalone sub process and detached into the background. ' .
+    'This is not possible or allowed on some systems (please check with your provider if ' .
+    'it is allowed to spawn background processes). This method has nearly no limitations.'
 );
 
 $GLOBALS['TL_LANG']['tl_settings']['composerVerbosityLevels']['VERBOSITY_QUIET'] = array(
