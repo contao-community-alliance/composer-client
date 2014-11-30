36fd1107c93974287d391a778661c6a8e7e2887f
Contao Community Alliance <info@c-c-a.org>
2014-11-30 03:00:54 +0100

    Auto-update translations from transifex

---
+++ src/system/modules/!composer/languages/rm/default.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/composer/language/rm/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-09-08T03:00:22+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['MSG']['disabled_by_composer'] = '(deactivà dal client da composer)';
+
---
+++ src/system/modules/!composer/languages/rm/tl_settings.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/composer/language/rm/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-09-08T03:00:23+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_settings']['composerAutoUpdateLibrary']['0']      = 'Actualisar automaticamain la blblioteca da composer';
+$GLOBALS['TL_LANG']['tl_settings']['composerAutoUpdateLibrary']['1']      = 'La biblioteca da composer (era enconuschenta sut il num <code>composer.phar</code>) vegn actualisada automaticamain suenter 30 dis.';
+$GLOBALS['TL_LANG']['tl_settings']['composerExecutionMode']['0']          = 'Modus d\'execuziun';
+$GLOBALS['TL_LANG']['tl_settings']['composerExecutionMode']['1']          = 'Tscherna co il binary da composer duai vegnir execuì.';
+$GLOBALS['TL_LANG']['tl_settings']['composerPhpPath']['0']                = 'Percurs/cummond per PHP';
+$GLOBALS['TL_LANG']['tl_settings']['composerPhpPath']['1']                = 'Il percurs u il cumond per la blbioteca dal binary da php.';
+$GLOBALS['TL_LANG']['tl_settings']['composerRemoveRepositoryTables']['0'] = 'Allontanar las tabellas dal client dal repository';
+$GLOBALS['TL_LANG']['tl_settings']['composerRemoveRepositoryTables']['1'] = 'Las tabellas dal client dal vegl repository ER2 na vegnan betg stizzadas da quest utensil d\'actualisaziun enfin che ti activeschas questa chaschetta.';
+
