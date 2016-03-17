eac822d022f53f649d93eb5adeedef3b43469cad
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-03-17 02:45:28 +0100

    Make false the composer autoupdate default value
    See issue #284 - automatically updating the composer.phar bears too many
    problems recently.

--- src/system/modules/!composer/config/config.php
+++ src/system/modules/!composer/config/config.php
@@ -34,7 +34,8 @@ define('COMPOSER_DIR_ABSOULTE', TL_ROOT . '/' . COMPOSER_DIR_RELATIVE);
 /**
  * Default configuration
  */
-$GLOBALS['TL_CONFIG']['composerAutoUpdateLibrary']      = true;
+// Do not update automatically. See #284.
+$GLOBALS['TL_CONFIG']['composerAutoUpdateLibrary']      = false;
 $GLOBALS['TL_CONFIG']['composerExecutionMode']          = 'inline';
 $GLOBALS['TL_CONFIG']['composerPhpPath']                =
     '/usr/bin/env php -d memory_limit=1G -d max_execution_time=900';
