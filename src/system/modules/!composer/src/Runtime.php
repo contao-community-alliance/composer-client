e4f405624bbcec99b82f4f7ef1bbc7b698811e3a
Christian Schiffler <c.schiffler@cyberspectrum.de>
2017-03-10 18:22:09 +0100

    Bump memory limit to 1.5G
    This is what it currently is set to in composer 1.4.x.

--- src/system/modules/!composer/src/Runtime.php
+++ src/system/modules/!composer/src/Runtime.php
@@ -283,9 +283,9 @@ EOF;
             };
 
             $memoryLimit = trim(ini_get('memory_limit'));
-            // Increase memory_limit if it is lower than 512M
-            if ($memoryLimit != -1 && $memoryInBytes($memoryLimit) < 1024 * 1024 * 1024) {
-                @ini_set('memory_limit', '1024M');
+            // Increase memory_limit if it is lower than 1.5GB
+            if ($memoryLimit != -1 && $memoryInBytes($memoryLimit) < 1024 * 1024 * 1536) {
+                @ini_set('memory_limit', '1536M');
             }
             unset($memoryInBytes, $memoryLimit);
         }
